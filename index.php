d0a502b395ac17dd6ee26e2be7428a8aa3078b3a
ssshah2131 <33388945+ssshah2131@users.noreply.github.com>
2024-03-20 23:07:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>POC by Shail Shah</title>
 </head>
 <body>
-    <h1>Hello Team! This is Subdomain Hijacking POC created by Shail Shah (shailshah1@kpmg.ccom)</h1>
+    <h1>Hello Team! This is Subdomain Hijacking POC created by Shail Shah (shailshah1@kpmg.com)</h1>
     <?php
         
     ?>
