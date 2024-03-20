bcec99b771f4c0b5358ad64d0a8de989a133dbe2
ssshah2131 <33388945+ssshah2131@users.noreply.github.com>
2024-03-20 22:44:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POC by Shail Shah</title>
+</head>
+<body>
+    <h1>Hello Team! This is Subdomain Hijacking POC created by Shail Shah (shailshah1@kpmg.ccom)</h1>
+    <?php
+        
+    ?>
+</body>
+</html>
